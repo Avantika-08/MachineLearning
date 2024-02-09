841de0b444b32fe44c79f8a533054216fc6e4f0a
Avantika-08 <avantikabisht860@gmail.com>
2024-02-09 19:28:07 +0530

    first commit

--- 1.py
+++ 1.py
@@ -1,1 +1,25 @@
-print(1+1)
+#Python datatype
+
+print(1+7)
+
+a=10
+print(type(a))
+
+print(type('Hello'))
+print(type(True))
+
+a=10
+b=20
+print(a*b)
+print((a*b)+(a/b))
+
+first_name="Avantika"
+last_name="Bisht"
+print("My name is {} {}".format(first_name,last_name))
+
+print("My name is {first} {last}".format(first=first_name,last=last_name))
+
+print(len(first_name))
+
+
+
