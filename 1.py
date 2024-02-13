e7312279ccec4ffbe537c900e52982c12ec80624
Avantika-08 <avantikabisht860@gmail.com>
2024-02-13 23:57:22 +0530

    first

--- 1.py
+++ 1.py
@@ -1,25 +1,25 @@
 #Python datatype
 
-print(1+7)
+# print(1+7)
 
-a=10
-print(type(a))
+# a=10
+# print(type(a))
 
-print(type('Hello'))
-print(type(True))
+# print(type('Hello'))
+# print(type(True))
 
-a=10
-b=20
-print(a*b)
-print((a*b)+(a/b))
+# a=10
+# b=20
+# print(a*b)
+# print((a*b)+(a/b))
 
-first_name="Avantika"
-last_name="Bisht"
-print("My name is {} {}".format(first_name,last_name))
+# first_name="Avantika"
+# last_name="Bisht"
+# print("My name is {} {}".format(first_name,last_name))
 
-print("My name is {first} {last}".format(first=first_name,last=last_name))
+# print("My name is {first} {last}".format(first=first_name,last=last_name))
 
-print(len(first_name))
+# print(len(first_name))
 
-
+print("Raju" in "ujaR") 
 
